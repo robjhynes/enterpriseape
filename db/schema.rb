1dbbcae5e575b10e7aec5d720724a9d0dfaf169b
robjhynes <rob_hynes@hotmail.com>
2015-03-24 11:56:09 +0000

    Add paid select drop down

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150106191331) do
+ActiveRecord::Schema.define(version: 20150324114816) do
 
   create_table "invoices", force: true do |t|
     t.datetime "date"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150106191331) do
     t.string   "salesperson"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "status_type"
   end
 
 end
